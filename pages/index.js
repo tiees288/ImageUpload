2bb6e9abc3ee2e83858442e63e280d09a0d9d5bb
Krittawat Boonchaiwattana <tiees288@gmail.com>
2021-03-27 20:31:20 +0700

    Upload Swal

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,88 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState } from 'react';
+import Swal from 'sweetalert2'
+// import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  let myfiles = null;
+  const [files, setfiles] = useState(null)
+
+  const handleFiles = (file) => {
+    setfiles(file)
+  }
+
+  const handleClear = () => {
+    setfiles(null)
+    console.log('Cleard.')
+  }
+
+  const handleUpload = (e) => {
+    e.preventDefault()
+
+    if (files == undefined) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Upload Fail',
+        // didOpen: () => {
+        //   Swal.showLoading()
+        // },
+        text: 'Something went wrong!',
+        // footer: '<a href>Why do I have this issue?</a>'
+      })
+    }
+    else {
+      Swal.fire({
+        // icon: 'error',
+        title: 'Upload in progress.',
+        didOpen: () => {
+          Swal.showLoading()
+        },
+        text: 'Please wait.!',
+        allowOutsideClick: false
+        // footer: '<a href>Why do I have this issue?</a>'
+      })
+
+
+      // Simulate Upload
+      setTimeout(() => {
+        Swal.close()
+      }, 1000);
+    }
+  }
+
+  if (files != undefined) {
+    console.log(files[0])
+    if (files[0] != undefined)
+      myfiles = URL.createObjectURL(files[0])
+    // console.log(myfiles)
+  }
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Upload Images</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <fieldset style={{ width: '500px' }}>
+        <legend>Upload Images</legend>
+        <form onSubmit={(e) => handleUpload(e)} >
+          <label>Files : </label>
+          <input type="file" accept="image/*" onChange={(e) => handleFiles(e.target.files)} />
+          <div style={{ textAlign: 'center' }}>
+            <button type="submit">Upload</button>
+            <button onClick={(e) => handleClear()} type="Reset">Clear</button>
+          </div>
+          <div>
+            {
+              files != null ? <img style={{ maxWidth: '150px', maxHeight: '150px' }} src={myfiles} /> : ''
+            }
+          </div>
+        </form>
+      </fieldset>
+      <fieldset style={{ width: '500px' }}>
+        <legend>Images List</legend>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </fieldset>
     </div>
   )
 }
